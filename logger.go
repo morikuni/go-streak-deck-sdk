59224ebb3b8be10c6c0e433cf2c6f7b72bd156fd
morikuni <taihei.with+git@gmail.com>
2022-02-12 15:18:41 +0900

    Use fmt.Sprint instead of Sprintln
    because Stream Deck automatically adds a line break

--- logger.go
+++ logger.go
@@ -8,12 +8,14 @@ type Logger struct {
 	conn *Conn
 }
 
+// Println prints log via Stream Deck API.
 func (l *Logger) Println(a ...interface{}) error {
 	return l.conn.Send(&LogMessage{
-		Message: fmt.Sprintln(a...),
+		Message: fmt.Sprint(a...),
 	})
 }
 
+// Printf prints formatted log via Stream Deck API.
 func (l *Logger) Printf(format string, a ...interface{}) error {
 	return l.conn.Send(&LogMessage{
 		Message: fmt.Sprintf(format, a...),
