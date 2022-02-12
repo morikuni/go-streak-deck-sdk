19370c6ee8711e42ac3e8f82c4383e966d688f45
morikuni <taihei.with+git@gmail.com>
2022-02-12 14:53:23 +0900

    Add manifest package

---
+++ manifest/manifest.go
@@ -0,0 +1,114 @@
+// Package manifest defines type of manifest.json.
+// The detailed document is here: https://developer.elgato.com/documentation/stream-deck/sdk/manifest/.
+package manifest
+
+import (
+	"encoding/json"
+)
+
+type Manifest struct {
+	Actions               []Action
+	Author                string
+	Category              *string `json:",omitempty"`
+	CategoryIcon          *string `json:",omitempty"`
+	CodePath              string
+	CodePathMac           *string `json:",omitempty"`
+	CodePathWin           *string `json:",omitempty"`
+	Description           string
+	Icon                  string
+	Name                  string
+	Profiles              []Profile `json:",omitempty"`
+	PropertyInspectorPath *string   `json:",omitempty"`
+	DefaultWindowSize     []int     `json:",omitempty"`
+	URL                   *string   `json:",omitempty"`
+	Version               string
+	SDKVersion            int
+	OS                    []OS
+	Software              Software
+	ApplicationsToMonitor *ApplicationsToMonitor `json:",omitempty"`
+}
+
+type Action struct {
+	Icon                    string
+	Name                    string
+	PropertyInspectorPath   *string `json:",omitempty"`
+	States                  []State
+	SupportedInMultiActions *bool   `json:",omitempty"`
+	Tooltip                 *string `json:",omitempty"`
+	UUID                    string
+	VisibleInActionsList    *bool `json:",omitempty"`
+}
+
+type State struct {
+	Image            string
+	MultiActionImage *string `json:",omitempty"`
+	Name             *string `json:",omitempty"`
+	Title            *string `json:",omitempty"`
+	ShowTitle        *bool   `json:",omitempty"`
+	TitleColor       *string `json:",omitempty"`
+	TitleAlignment   *string `json:",omitempty"`
+	FontFamily       *string `json:",omitempty"`
+	FontStyle        *string `json:",omitempty"`
+	FontSize         *int    `json:",omitempty"`
+	FontUnderline    *bool   `json:",omitempty"`
+}
+
+type Profile struct {
+	Name                        string
+	DeviceType                  DeviceType
+	ReadOnly                    *bool `json:",omitempty"`
+	DontAutoSwitchWhenInstalled *bool `json:",omitempty"`
+}
+
+type DeviceType int
+
+const (
+	DeviceTypeStreamDeck       DeviceType = 0
+	DeviceTypeStreamDeckMini   DeviceType = 0
+	DeviceTypeStreamDeckXL     DeviceType = 0
+	DeviceTypeStreamDeckMobile DeviceType = 0
+	DeviceTypeCorsairGKeys     DeviceType = 0
+	DeviceTypeStreamDeckPanel  DeviceType = 0
+)
+
+type OS struct {
+	Platform       Platform
+	MinimumVersion string
+}
+
+type Platform string
+
+const (
+	PlatformMac     Platform = "mac"
+	PlatformWindows Platform = "windows"
+)
+
+type Software struct {
+	MinimumVersion string
+}
+
+type ApplicationsToMonitor struct {
+	Mac     []string
+	Windows []string
+}
+
+func (a ApplicationsToMonitor) MarshalJSON() ([]byte, error) {
+	type obj struct {
+		Mac     []string `json:"mac"`
+		Windows []string `json:"windows"`
+	}
+	o := obj(a)
+	return json.Marshal(o)
+}
+
+func OptionalString(s string) *string {
+	return &s
+}
+
+func OptionalBool(b bool) *bool {
+	return &b
+}
+
+func OptionalInt(i int) *int {
+	return &i
+}
