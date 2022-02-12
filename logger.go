2ba1f7ccd7b355a4302186d3a32301d2fb4bab61
morikuni <taihei.with+git@gmail.com>
2022-02-13 00:29:48 +0900

    Simulate fmt.Println

--- logger.go
+++ logger.go
@@ -10,8 +10,9 @@ type Logger struct {
 
 // Println prints log via Stream Deck API.
 func (l *Logger) Println(a ...interface{}) error {
+	s := fmt.Sprintln(a...)
 	return l.conn.Send(&LogMessage{
-		Message: fmt.Sprint(a...),
+		Message: s[:len(s)-1],
 	})
 }
 
